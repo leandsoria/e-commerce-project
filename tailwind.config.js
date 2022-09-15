051a2bc6b0c52b683faf146f0606c1288c79071e
leandsoria <lean.dsoria@gmail.com>
2022-09-15 03:19:02 -0300

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
